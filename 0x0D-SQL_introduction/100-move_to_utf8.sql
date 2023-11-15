9addc9a3b9d002d8f584b52d83d4b805afd2b084
Mawuli Agropah <wizzyagrops2016@gmail.com>
2023-11-15 03:33:22 +0000

    done

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++
@@ -1,9 +0,0 @@
-/*a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_c)*/
--- Convert the database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to utf8mb4
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the field (name) to utf8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
